fe3361297cd841569cb208930be6c353b0d8969d
Joshua Mayer <jpmayerunc@gmail.com>
2016-09-10 15:41:12 -0700

    Add files via upload

---
+++ templates/fourteenTeamLeague.py
@@ -0,0 +1,107 @@
+# Fill in the form below with info about your league managers
+# 'name' is there name, this will be a lookup key in the DB, so make sure its the same name you use when importing the data
+# 'nickname' is a shortened version of their name that will show up in the power rankings
+# 'picture' is a direct url to the picture you want to represent the manager in the power rankings
+# 'clubHouse' is the url link to the managers team page
+# 'lastYearPosition' is the position in which the player finished last year (ex. "1st") ("N/A" if no results from last year)
+
+managerInfo = [
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      },
+      {
+        'name': ,
+        'nickname': ,
+        'picture': ,
+        'clubHouse': ,
+        'lastYearPosition':
+      }
+]
